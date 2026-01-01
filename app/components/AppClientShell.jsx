fd21eac13521b2668ccaac4fb54a3bad9bdd6dfb
Camilla Lauenborg <camillalauenborg@camillas-mbp-3.lan>
2026-01-01 13:19:52 +0100

    Add auth topbar while keeping desktop sidebar nav

--- app/components/AppClientShell.jsx
+++ app/components/AppClientShell.jsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import Nav from './Nav'
 import AuthModal from './AuthModal'
+import AuthTopBar from './AuthTopBar'
 
 export default function AppClientShell({ children }) {
   const [authOpen, setAuthOpen] = useState(false)
@@ -10,12 +11,14 @@ export default function AppClientShell({ children }) {
 
   return (
     <>
-      <Nav
+      <AuthTopBar
         openAuth={(mode) => {
           setAuthMode(mode)
           setAuthOpen(true)
         }}
       />
+
+      <Nav />
 
       <AuthModal
         open={authOpen}
